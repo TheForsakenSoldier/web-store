6f7e584ddd4e9532c4396bbc16c5c713a89861ad
TheForsakenSoldier <IFlearnandwork@protonmail.com>
2022-07-03 14:32:24 +0300

    finishing touches with navigation concepts ready to merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
-import NavBar from "./components/routes/navigation-bar/navigation-bar.component";
+import NavBar from "./components/routes/navigation/navigation.component";
+import Shop from "./components/routes/shop/shop.component";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<NavBar/>}>
         <Route index element={<Home />}/>
+        <Route path="/shop" element={<Shop/>} />
       </Route>
     </Routes>
   );
